0b96e49f885b2b5fbb186fe0abc961be3da58dd0
Trevor Cickovski <tcickovs@delphi.cs.fiu.edu>
2022-04-12 22:49:04 -0400

    Added more BLAST command line options

--- BLASTNPlugin.cpp
+++ BLASTNPlugin.cpp
@@ -24,6 +24,7 @@ void BLASTNPlugin::output(std::string file) {
 
  //blastp -query "+organism1+".faa -db "+organism2+".db -outfmt 6 > "+organism1+"_"+organism2+".txt"
  //makeblastdb -in PA14.txt -out PA14.db
+ //blastn -db release10_bac_$i -query primers.txt -num_descriptions 50000 -num_alignments 50000 -evalue 50 -dust no -soft_masking false -word_size 8
 myCommand += "blastn";
 myCommand += " ";
 myCommand += "-query";
@@ -35,11 +36,42 @@ myCommand += std::string(PluginManager::prefix()) + "/" + parameters["db"] + " "
 if (parameters.count("outfmt") != 0) {
 myCommand += "-outfmt";
 myCommand += " ";
-}
 myCommand += parameters["outfmt"] + " ";
+}
+if (parameters.count("num_descriptions") != 0) {
+myCommand += "-num_descriptions";
+myCommand += " ";
+myCommand += parameters["num_descriptions"] + " ";
+}
+if (parameters.count("num_alignments") != 0) {
+myCommand += "-num_alignments";
+myCommand += " ";
+myCommand += parameters["num_alignments"] + " ";
+}
+if (parameters.count("evalue") != 0) {
+myCommand += "-evalue";
+myCommand += " ";
+myCommand += parameters["evalue"] + " ";
+}
+if (parameters.count("dust") != 0) {
+myCommand += "-dust";
+myCommand += " ";
+myCommand += parameters["dust"] + " ";
+}
+if (parameters.count("soft_masking") != 0) {
+myCommand += "-soft_masking";
+myCommand += " ";
+myCommand += parameters["soft_masking"] + " ";
+}
+if (parameters.count("word_size") != 0) {
+myCommand += "-word_size";
+myCommand += " ";
+myCommand += parameters["word_size"] + " ";
+}
 myCommand += ">";
 myCommand += " ";
 myCommand += outputfile;
+std::cout << myCommand << std::endl;
  system(myCommand.c_str());
 }
 
